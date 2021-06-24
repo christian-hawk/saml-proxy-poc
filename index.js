d8bccf15a572b0daf79d3b142b3070baa9e9f0ee
christian-hawk <eland.christian@gmail.com>
2021-06-24 18:34:41 +0000

    feat: add ssl certificates to server

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const app = require('./app')
-const https = require('http')
+const https = require('https')
 const config = require('config')
 const logger = require('./logger')
+const fs = require('fs')
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
 
 const port = config.get('server.port')
-
 logger.info(`setting app port ${port}`)
 app.set('port', port)
 
-const server = https.createServer(app)
+const options = {
+    key: fs.readFileSync('ssl/privkey.pem'),
+    cert: fs.readFileSync('ssl/fullchain.pem'),
+    ca: fs.readFileSync('ssl/chain.pem')
+}
+
+const server = https.createServer(options, app)
 
 server.listen(port, () => {
     logger.info('server started...')
